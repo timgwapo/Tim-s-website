520f4239ca4c4a6d1ab4d0ff89b0e0290aa8da52
Tim <132417688+timgwapo@users.noreply.github.com>
2024-09-09 10:35:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+})
+
+const hiddenClasses = document.querySelectorAll('.hidden,.hiddenRev, .rolling, .hiddenAbove');
+hiddenClasses.forEach((el) => observer.observe(el));
+
+document.querySelector('.burger-menu').addEventListener('click', function() {
+    document.querySelector('.header-menu-bar').classList.toggle('active');
+});
+
+const form = document.querySelector("form");
+
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phoneNum = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phoneNum.value}<br> Message: ${mess.value}`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "timothylhernandez23@gmail.com",
+        Password : "1E04A16E9C509551381C9C7B083F9A762FBE",
+        To : 'timothylhernandez23@gmail.com',
+        From : "timothylhernandez23@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert("successfully sent")
+    );
+}
